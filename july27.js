be8eae4010d3cd58b7ca6f4b3d4e04a843594693
Hannah <hrglasser94@gmail.com>
2018-07-27 13:53:27 -0400

    Started steamroll array function.

--- july27.js
+++ july27.js
@@ -2,8 +2,27 @@
 //Flatten a nested array. You should account for various levels of nesting. 
 //steamrollArray([1, [2], [3, [[4]]]]); should return [1,2,3,4]
 
+
+//example:
+// var greet = function(i) {
+//     if (i !== 0) {
+//         console.log('greetings!');
+//         greet(i-1);
+//     }
+// }
+
+// greet(5);
+var newArray = [];
+
 function steamrollArray(arr) {
-    return arr;
+    debugger;
+    var index = arr.length - 1;
+    if ((typeof arr[index]) !== number) {
+        steamrollArray(arr[index - 1]);
+    }
+    
+    newArray.push(arr[index]);
+    return newArray;
 }
 
 steamrollArray([1, [2], [3, [[4]]]]);
